1e2301e36d646d9f57f6b6c0fc12ff65f98c9653
jopdel <opdelochtjohn@gmail.com>
2024-01-15 15:32:02 +0100

    Reorganised routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { StockComponent } from './stock/stock.component';
 import { StockaddComponent } from './stockadd/stockadd.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-// import { IngredientComponent } from './ingredient/ingredient.component';
-// import { IngredientaddComponent } from './ingredientadd/ingredientadd.component';
-// import { IngredienteditComponent } from './ingredientedit/ingredientedit.component';
+import { IngredientComponent } from './ingredient/ingredient.component';
+import { IngredientaddComponent } from './ingredientadd/ingredientadd.component';
+import { IngredienteditComponent } from './ingredientedit/ingredientedit.component';
 import { StockeditComponent } from './stockedit/stockedit.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {
@@ -27,23 +28,28 @@ export const routes: Routes = [
         component: StockaddComponent
     },
     {
-        path: 'register',
-        component: RegisterComponent
-    },
-    // {
-    //     path: 'ingredient',
-    //     component: IngredientComponent
-    // },
-    // {
-    //     path: 'ingredientadd',
-    //     component: IngredientaddComponent
-    // },
-    // {
-    //     path: 'ingredientedit',
-    //     component: IngredienteditComponent
-    // },
-    {
         path: 'stockedit',
         component: StockeditComponent
+    },
+    {
+        path: 'ingredient',
+        component: IngredientComponent
+    },
+    {
+        path: 'ingredientadd',
+        component: IngredientaddComponent
+    },
+    {
+        path: 'ingredientedit',
+        component: IngredienteditComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
     }
 ];
