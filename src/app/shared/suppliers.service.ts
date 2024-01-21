ef58d0dbf52e5f7cb3b5e9a3f71a772c8a794a8f
jopdel <opdelochtjohn@gmail.com>
2024-01-21 09:19:58 +0100

    added a suppliers service

--- src/app/shared/suppliers.service.ts
+++ src/app/shared/suppliers.service.ts
@@ -6,4 +6,13 @@ import { Injectable } from '@angular/core';
 export class SuppliersService {
 
   constructor() { }
+  // put suppliers API Endpoint URL in constant
+  suppliersURL: string = 'http://127.0.0.1:8000/api/suppliers'
+
+  // get suppliers
+  async getSuppliers() {
+    return (await fetch(this.suppliersURL)).json()
+  }
+
+
 }
